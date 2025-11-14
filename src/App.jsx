a55eda93784d70f7eedfa572bdc8980c8261ef5f
ousemaakacha <akachaousema@gmail.com>
2025-11-14 12:23:34 +0100

    feat: strutcture for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,29 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
+      <nav className="">
+        <div className="">
+          <span>MyStore</span>
+          <button>
+            <span></span>
+          </button>
+          <div>
+            <ul>
+              <li>
+                <span>Home</span>
+              </li>
+
+              <li>
+                <span>About Us</span>
+              </li>
+
+              <li>
+                <span>Products</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
