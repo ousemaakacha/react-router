3bdc649ea24d5ae1263252e11108d5778e5021cb
ousemaakacha <akachaousema@gmail.com>
2025-11-14 12:12:56 +0100

    feat: routes for home, prod and about

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-function App() {
-  const [count, setCount] = useState(0);
+import { Route, Routes } from "react-router-dom";
 
-  return <></>;
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
