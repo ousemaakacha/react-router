bf8e4de03483e52499201812d2eb1874464b8d95
ousemaakacha <akachaousema@gmail.com>
2025-11-16 21:54:35 +0100

    feat: axios call for products api. feat: products cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          {/* <Route path="/products" element={<Products />} /> */}
+          <Route path="/products" element={<Products />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/Products.jsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function Products() {
+  const [products, setProducts] = useState([]);
+  const endProd = "https://fakestoreapi.com/products";
+  function getProd() {
+    axios
+      .get(endProd)
+      .then((res) => {
+        console.log(res);
+        setProducts(res.data);
+      })
+      .catch((err) => console.error(err));
+  }
+
+  useEffect(getProd, []);
+  return (
+    <>
+      <div className="container">
+        <h1 className="mb-4">Products</h1>
+
+        <div className="row g-4">
+          {products.map((product) => (
+            <div key={product.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
+              <div className="card h-100">
+                <img
+                  src={product.image}
+                  className="card-img-top p-3"
+                  style={{ height: "200px", objectFit: "contain" }}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">
+                    {product.title.slice(0, 40)}...
+                  </h5>
+                  <p className="card-text fw-bold">{product.price} €</p>
+                </div>
+                <div className="card-footer text-center">
+                  <button className="btn btn-primary">About the product</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
