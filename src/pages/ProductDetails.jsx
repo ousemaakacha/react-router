1de5fcd10134089ef43c0d021044c746872db4b3
ousemaakacha <akachaousema@gmail.com>
2025-11-17 12:57:13 +0100

    feat: function for useEffect

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,9 +1,24 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { data, useParams } from "react-router-dom";
 
 export default function ProductDetail() {
   const [product, setProduct] = useState([]);
   const { id } = useParams();
+
+  function getProdDet() {
+    axios
+      .get(`https://fakestoreapi.com/products/${id}`)
+      .then((res) => {
+        console.log(res);
+        setProduct(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  useEffect(getProdDet, []);
 
   return <></>;
 }
